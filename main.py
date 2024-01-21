39244013649c1da8883c1719a82ac9ab66a11533
Aronk-112 <aronkvaszta12@outlook.com>
2024-01-21 15:18:31 +0100

    Done without Refs

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 import flet as ft
 
 TITLE: str = 'Greeings App'
+WIDTH: int = 625
+HEIGHT: int = 400
 
 
 def main(page: ft.Page):
     page.title = TITLE
+
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    page.window_width = WIDTH
+    page.window_height = HEIGHT
+
+    page.window_resizable = False
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    def toggle_mode(e):
+        if page.theme_mode == ft.ThemeMode.DARK:
+            page.theme_mode = ft.ThemeMode.LIGHT
+            theme_button.icon = ft.icons.LIGHT_MODE
+            print("Light mode")
+        elif page.theme_mode == ft.ThemeMode.LIGHT:
+            page.theme_mode = ft.ThemeMode.DARK
+            theme_button.icon = ft.icons.DARK_MODE
+            print("Dark mode")
+
+        page.update()
 
     def say_hello(e):
-        print("Hello, world!")
+        first_name: str = first_name_field.value
+        last_name: str = last_name_field.value
+
+        first_name = first_name.strip(' ').title()
+        last_name = last_name.strip(' ').title()
+
+        if first_name == '' or last_name == '':
+            return
+
+        greeting.value = f"Hello, {first_name} {last_name}!"
+        greeting.update()
 
     # TITLE
     page_title_text: ft.Text = ft.Text(
         value=TITLE, size=36, font_family='arial', weight=600
+    )
+
+    theme_button: ft.IconButton = ft.IconButton(
+        icon=ft.icons.LIGHT_MODE,
+        on_click=toggle_mode
     )
 
     # NAME
@@ -32,7 +69,6 @@ def main(page: ft.Page):
 
     # ON SEND REVEAL
     greeting: ft.Text = ft.Text(
-        value="Hello [first_name] [last_name]",
         font_family='arial',
         size=36,
         weight=800
@@ -40,11 +76,11 @@ def main(page: ft.Page):
 
     page.add(
         ft.Column([
-            ft.Row([page_title_text], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([first_name_field], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([last_name_field], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([send_button], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([greeting], alignment=ft.MainAxisAlignment.CENTER)
+            ft.Row([page_title_text, theme_button]),
+            first_name_field,
+            last_name_field,
+            send_button,
+            greeting
         ])
     )
 
